0042d547738249f48c4976611c0a8a653094e8f2
theparadox00-ai <suryadiya04@gmail.com>
2026-02-21 11:07:59 +0530

    Update sd_data.h

--- debugger/inc/sd_data.h
+++ debugger/inc/sd_data.h
@@ -7,10 +7,13 @@ extern const char* DIR_LOADCELL;
 extern const char* DIR_SHT;
 extern const char* DIR_BC;
 extern const char* DIR_PARAM;
+extern const char* DIR_TEMP;
 
 bool Init_SD();
 void LoadCell_write(char* time, float load);
 void SHT_write(char* time, float temp, float humi);
+void LoadCell_Temp_write(char* time, float load);
+void SHT_Temp_write(char* time, float temp, float humi);
 void parameter(char* time);
 void SD_Sleep();
 
